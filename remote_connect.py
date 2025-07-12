fea8225fd3fcb32e6ec1e6609725abd37614a6ed
William <121920056+QinWilliam@users.noreply.github.com>
2025-07-12 15:59:53 +0800

    Update remote_connect.py

--- remote_connect.py
+++ remote_connect.py
@@ -4,10 +4,7 @@ ssh_client = paramiko.SSHClient()
 ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 def connect(host,user,password):
     ssh_client.connect(hostname=host, username=user, password=password)
-    # stdin, stdout, stderr = ssh_client.exec_command('ls /')
-    # print(stdout.read().decode("utf-8"))
-    # ssh_client.close()
-    # print(f"{user}@{host} is connected")
+    
     return host
 def disconnect():
     print("SSH is disconnected")
